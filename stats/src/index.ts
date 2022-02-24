85b8257ef2a68649b528b7fa859cc743af849b7c
AlexB <alex.i.bajan@gmail.com>
2022-02-24 21:49:59 +0200

    enums - introdction

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -10,12 +10,18 @@ const matches = fs
     return row.split(',');
   });
 
+enum matchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
 // Analyze data
 let manUnitedWins = 0;
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === matchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === matchResult.AwayWin) {
     manUnitedWins++;
   }
 }
