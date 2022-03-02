c9a3b7f9dc47699bc522f8833b0c9a930fa44319
AB <alex.i.bajan@gmail.com>
2022-03-02 20:49:53 +0200

    advanced generic constraint

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -12,14 +12,4 @@ const rooUrl = 'http://localhost:300-/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rooUrl);
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 }
