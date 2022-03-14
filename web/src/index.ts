8885cf985d1647723a36bad9adae98d30b7a0a1c
Ion Bajan <ion.bajan@uti.ro>
2022-03-14 20:29:30 +0200

    final refactor

--- web/src/index.ts
+++ web/src/index.ts
@@ -12,10 +12,11 @@
 
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'newer name', age: 0 });
+const user = User.buildUser({ id: 2 });
 
-user.on('save', () => {
+user.on('change', () => {
   console.log(user);
 });
 
-user.save();
+// user.save();
+user.fetch();
