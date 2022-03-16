57497b003a7fcaac02338313fa683aeff0f45056
AB <alex.i.bajan@gmail.com>
2022-03-16 23:23:33 +0200

    use case for the event framework

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,8 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'NAME', age: 28 });
+
+const userForm = new UserForm(document.getElementById('root'), user);
+
 userForm.render();
--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,27 +1,35 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onMouseEnter,
+      'click:.set-age': this.onSetAgeClick,
     };
   }
 
-  onMouseEnter(): void {
-    console.log('mouse enter event triggered');
-  }
-
-  onButtonClick(): void {
-    console.log('click event triggered');
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
     <div>
       <h1>User Input</h1>
+      <div>User name: ${this.model.get('name')}</div>
+      <div>User age: ${this.model.get('age')}</div>
       <input />
       <button>Click Me</button>
+      <button class="set-age">Set Random Age</button>
     </div>
     `;
   }
@@ -39,6 +47,7 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
